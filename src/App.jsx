56f8b86409ea8d0d485a7a56bd5afe14d367f038
Prateek Kumar <coolprateek.kumar@gmail.com>
2021-04-22 23:04:05 +0530

    Setup eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-const App = () => <div>Hello</div>;
+import './App.css';
+
+const App = () => (
+  <div className="grid-container">
+    <div className="nav">Nav</div>
+    <div className="main">Main</div>
+    <div className="footer">Footer</div>
+  </div>
+);
 
 export default App;
